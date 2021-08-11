b51138e04032ffc7d7af20e5e4ed8e1db1133a7a
Eduardo Graminho <edugraminho@gmail.com>
2021-08-11 09:57:23 -0300

    project base

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color:#8257e6;
+  font-size: 50px;
+`
+
+
+export function App() {
+  return (
+    <div className="App">
+      <Title>Hello Word</Title>
+    </div>
+  );
+}
+
